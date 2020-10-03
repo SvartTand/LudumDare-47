8bea4d15600933182a2b27bf08d24b35844b5d5a
SvartTand <albert24lindberg@gmail.com>
2020-10-03 11:30:30 +0300

    Added movement

---
+++ LudumDare47/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject follow;
+
+    private Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = follow.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position =new Vector3(t.position.x, t.position.y, transform.position.y);
+    }
+}
---
+++ LudumDare47/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float jumpForce;
+    public float rotForce;
+
+    private float distToground;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distToground = gameObject.GetComponent<Collider2D>().bounds.extents.y*2;
+        Debug.Log(gameObject.GetComponent<Collider2D>().bounds.extents.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * rotForce, 0));
+        rb.AddForce(new Vector2(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotForce));
+
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+        
+    }
+
+    private bool IsGrounded()
+    {
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, distToground);
+        
+
+        Debug.Log(hit.collider.tag);
+
+        return Physics2D.Raycast(transform.position, -Vector2.up, distToground);
+    }
+}
